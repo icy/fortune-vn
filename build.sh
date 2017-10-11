f86c3397a19b4727771a64d71f6fae7a55eeb16d
Stephen Truong Trung Hieu <trunghieu.truong.bit@gmail.com>
2017-10-11 18:08:34 +0700

    build.sh: pack all file and compile

--- build.sh
+++ build.sh
@@ -1,9 +1,16 @@
 #! /usr/bin/env bash
 
-rm -f data/*.dat
+rm -f data/all.*
+touch data/all.txt
 
-for file in data/*; do
-    strfile -c % ${file} ${file}.dat
+for file in data/*[^all].txt; do
+    if [[ $(tail -n 1 ${file}) != "%" ]]; then
+        echo % >> ${file}
+    fi
+
+    cat ${file} >> data/all.txt
 done
+
+strfile -c % data/all.txt data/all.txt.dat
 
 echo Done, Have fun!
